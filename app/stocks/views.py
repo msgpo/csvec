c43107196020d64d65c57344383a1757239838ab
origin-unknown <>
2020-07-21 01:29:59 +0200

    fixed bugs

--- app/stocks/views.py
+++ app/stocks/views.py
@@ -24,16 +24,15 @@ def index():
         source = request.form.get('source', 'yahoo')
         if symbol:
             df = pdr.DataReader(symbol, source, '2020-01-01')
-            chart = make_chart(df, source, symbol, 30)
+            if source == 'yahoo': chart = make_chart(df, symbol, 30)
             data = df.to_html(classes='table table-striped table-hover')
     return render_template('stocks/index.html', data=data, chart=chart)
 
 
-def make_chart(data_frame, stype, symbol, days):
+def make_chart(data_frame, symbol, days):
     head = [pd.to_datetime(str(d)).strftime('%Y-%m-%d') \
         for d in data_frame[-days:].index.values]
-    data = [d[-1 if stype == 'yahoo' else -2] \
-        for d in data_frame[-days:].values]
+    data = [d[-1] for d in data_frame[-days:].values]
 
     line_chart = pygal.Line(x_label_rotation=35)
     line_chart.title = f'Price of the last {days} trading days'
